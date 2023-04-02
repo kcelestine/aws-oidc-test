f172dec3c211f86cedf7d6923771216ae757612a
Khadijah <khadijah.celestine@gmail.com>
2023-04-01 21:40:19 -0400

    trying to format properly with no ide -_-

--- main.tf
+++ main.tf
@@ -5,30 +5,11 @@ terraform {
       version = "4.61.0"
     }
   }
-  
   cloud {
     organization = "_cloudcte"
 
     workspaces {
       name = "aws-terraform-github-actions"
     }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_vpc" "main2" {
-  cidr_block = "10.1.0.0/16"
-}
-
-resource "null_resource" "example" {
-  triggers = {
-    value = "A example resource that does nothing!"
   }
 }

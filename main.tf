9743c1ca1130cfdfdf7f7d0de8ef6de98a232919
Khadijah <khadijah.celestine@gmail.com>
2023-04-01 21:52:11 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,33 +1,8 @@
 terraform {
-  cloud {
-    organization = "_cloudcte"
-    workspaces {
-      name = "aws-oidc-test"
-    }
-  }
-  
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "3.74"
+      version = "4.61.0"
     }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_vpc" "main2" {
-  cidr_block = "10.1.0.0/16"
-}
-
-resource "null_resource" "example" {
-  triggers = {
-    value = "A example resource that does nothing!"
   }
 }

49c5fd6bb0756271f85f95a68e062d7158764a3a
Khadijah <khadijah.celestine@gmail.com>
2023-04-01 22:38:01 -0400

    TFC_AWS_PROVIDER_AUTH

--- main.tf
+++ main.tf
@@ -31,3 +31,8 @@ variable "TFC_AWS_RUN_ROLE_ARN" {
   type        = string
   description = "TFC_AWS_RUN_ROLE_ARN"
 }
+
+variable "TFC_AWS_PROVIDER_AUTH" {
+  type        = boolean
+  description = "TFC_AWS_PROVIDER_AUTH"
+}

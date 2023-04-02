e79f2325ac312f6b926b3fabd476eeb3552b407c
Khadijah <khadijah.celestine@gmail.com>
2023-04-01 21:35:35 -0400

    back to the original main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,9 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.61.0"
-    }
-  }  
-  
   cloud {
     organization = "_cloudcte"
 
     workspaces {
-      name = "aws-oidc-test"
+      name = "aws-terraform-github-actions"
     }
   }
 }

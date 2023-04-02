b356facfd8dc32bda7679b8ccd9272c35da84349
Khadijah <khadijah.celestine@gmail.com>
2023-04-01 22:33:47 -0400

    starting over gain

--- main.tf
+++ main.tf
@@ -1,8 +1,18 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.61.0"
+  cloud {
+    organization = "_cloudcte"
+
+    workspaces {
+      name = "aws-oidc-test"
     }
   }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }

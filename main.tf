a9e54bffddfa9fbd805a5a7cab4af6a821f43026
Khadijah <khadijah.celestine@gmail.com>
2023-04-01 21:43:10 -0400

    wtf ever. starting over. again

--- main.tf
+++ main.tf
@@ -1,15 +1,28 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.61.0"
-    }
-  }
   cloud {
     organization = "_cloudcte"
 
     workspaces {
-      name = "aws-terraform-github-actions"
+      name = "aws-oidc-test"
     }
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_vpc" "main2" {
+  cidr_block = "10.1.0.0/16"
+}
+
+resource "null_resource" "example" {
+  triggers = {
+    value = "A example resource that does nothing!"
   }
 }

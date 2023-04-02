2d36ecc6f39e2f6ba96e0e6dde2ca27eb5f7afab
Khadijah <khadijah.celestine@gmail.com>
2023-04-01 22:36:38 -0400

    adding oidc vars

--- main.tf
+++ main.tf
@@ -16,3 +16,18 @@ provider "aws" {
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
+
+variable "TFC_AWS_PLAN_ROLE_ARN" {
+  type        = string
+  description = "TFC_AWS_PLAN_ROLE_ARN"
+}
+
+variable "TFC_AWS_APPLY_ROLE_ARN" {
+  type        = string
+  description = "TFC_AWS_APPLY_ROLE_ARN"
+}
+
+variable "TFC_AWS_RUN_ROLE_ARN" {
+  type        = string
+  description = "TFC_AWS_RUN_ROLE_ARN"
+}

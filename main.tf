38629a8cdedf34464d6468828444f26f70626e88
Khadijah <khadijah.celestine@gmail.com>
2023-04-02 01:31:10 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,3 +16,23 @@ provider "aws" {
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
+
+variable "TFC_AWS_PLAN_ROLE_ARN" {
+  type        = string
+  description = "TFC_AWS_PLAN_ROLE_ARN"
+}
+
+variable "TFC_AWS_APPLY_ROLE_ARN" {
+  type        = string
+  description = "TFC_AWS_APPLY_ROLE_ARN"
+}
+
+variable "TFC_AWS_RUN_ROLE_ARN" {
+  type        = string
+  description = "TFC_AWS_RUN_ROLE_ARN"
+}
+
+variable "TFC_AWS_PROVIDER_AUTH" {
+  default     = true
+  description = "TFC_AWS_PROVIDER_AUTH"
+}
